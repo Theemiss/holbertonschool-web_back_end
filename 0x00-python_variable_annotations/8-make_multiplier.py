a507942d278c9e2ff0b0e334a74636f89354b2d8
Theemiss <midinfotn401@gmail.com>
2022-09-05 13:28:36 +0100

    T7

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 """
 # 7-to_kv module"""
+from typing import Callable
 
 
-def make_multiplier(multiplier: float) -> callable:
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
     """
     make_multiplier function"""
     def multiplier(multiplier):
