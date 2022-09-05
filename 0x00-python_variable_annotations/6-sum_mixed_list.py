bb8d60170c90159f68cc9f27443d2561c1ecad86
Theemiss <midinfotn401@gmail.com>
2022-09-05 13:08:43 +0100

    T6

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+"""
+# 6-sum_mixed_list module"""
+from typing import Union, List
+
+
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+    """Returns the sum of all integers in a list"""
+    return sum(mxd_lst)
