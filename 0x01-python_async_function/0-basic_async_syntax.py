c341622b344a71a84541e14e24edfa5095daa67a
Theemiss <midinfotn401@gmail.com>
2022-09-05 14:55:51 +0100

    added variable anotation'

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -3,7 +3,7 @@
 import asyncio
 
 
-async def wait_random(max_delay=10):
+async def wait_random(max_delay: int =10) -> float:
     """wait_random: takes in an integer argument (max_delay, with a default value of 10) named wait_random that waits for a random delay between 0 and max_delay (included and float value) seconds and eventually returns it.
     """
     import asyncio
