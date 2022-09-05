7bdfcbce4153344edfaf498d22365d7b92fd8dff
Theemiss <midinfotn401@gmail.com>
2022-09-05 13:04:30 +0100

    T5

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""
+5-sum_list module"""
+
+
+def sum_list(input_list: list[float]) -> float:
+    """Returns the sum of all integers in a list"""
+    return sum(input_list)
