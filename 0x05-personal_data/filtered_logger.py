282d7a598a3036116fe663b002b4adbfd8372403
Theemiss <midinfotn401@gmail.com>
2022-09-13 14:46:21 +0100

    logger

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -2,15 +2,32 @@
 """
 obscure log messages
 """
-
+import logging
 import re
 from typing import List
 
 
-def filter_datum(fields: List[str], redaction: str,
-                 message: str, separator: str) -> str:
-    """Returns the log message obfuscated"""
-    for field in fields:
-        message = re.sub(fr'{field}=.*?{separator}',
-                         f'{field}={redaction}{separator}', message)
-    return message
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """Returns the log message obfuscated"""
+        return self.filter_datum(self.REDACTION,
+                                 super().format(record), self.SEPARATOR)
+
+    def filter_datum(self, redaction: str,
+                     message: str, separator: str) -> str:
+        """Returns the log message obfuscated"""
+        for field in self.fields:
+            message = re.sub(fr'{field}=.*?{separator}',
+                             f'{field}={redaction}{separator}', message)
+        return message
