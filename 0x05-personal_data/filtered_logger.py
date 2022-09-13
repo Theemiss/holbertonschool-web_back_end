c5b71f682ddbec6023406e4a0ac0cf6bff4a824f
Theemiss <midinfotn401@gmail.com>
2022-09-13 15:19:58 +0100

    connection to database

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -46,7 +46,7 @@ def get_logger() -> logging.Logger:
     return logger
 
 
-def get_db():
+def get_db() -> mysql.connector.connection.MySQLConnection:
     """
     Returns a connector to the database
     """
