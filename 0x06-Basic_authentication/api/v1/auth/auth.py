b526f6e174c2f62b9f4b58ad787ac4c7fa7ad980
Themis <midinfotn401@gmail.com>
2022-10-04 12:44:16 +0100

    auth class

---
+++ 0x06-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,28 @@
+from flask import request
+from typing import List, TypeVar
+"""
+This module contains the Auth class
+"""
+
+
+class Auth():
+    """
+    """
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """
+        return True if the path is not in the list of strings excluded_paths
+        """
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """
+        authorization_header
+        """
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """
+        current_user
+        """
+        return None
