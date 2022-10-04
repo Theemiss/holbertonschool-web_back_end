508e697b1f2e3f585ad940569cd9f78e0d057481
Themis <midinfotn401@gmail.com>
2022-10-04 11:34:15 +0100

    init

--- 0x06-Basic_authentication/api/v1/views/index.py
+++ 0x06-Basic_authentication/api/v1/views/index.py
@@ -29,5 +29,6 @@ def stats() -> str:
 @app_views.route("/unauthorized", methods=["GET"], strict_slashes=False)
 def unauthorized() -> str:
     """
+    GET /api/v1/unauthorized
     """
     abort(401)
