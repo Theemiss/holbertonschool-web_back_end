2a5299b64aed5d43861fe1fa4401b98e450e109a
Theemiss <midinfotn401@gmail.com>
2022-10-18 10:14:47 +0100

    update  for test

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -2,6 +2,7 @@
 """
 obscure log messages
 """
+from asyncio.log import logger
 import logging
 import re
 from typing import List
@@ -10,51 +11,76 @@ import os
 PII_FIELDS = ('name', 'email', 'phone', 'ssn', 'password')
 
 
-class RedactingFormatter(logging.Formatter):
-    """ Redacting Formatter class
-        """
-    REDACTION = "***"
-    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
-    SEPARATOR = ";"
+def filter_datum(fields: List[str],
+                 redaction: str,
+                 message: str,
+                 separator: str) -> str:
+    """Returns the log message obfuscated"""
+    for field in fields:
+        message = re.sub(fr'{field}=.*?{separator}',
+                         f'{field}={redaction}{separator}', message)
+    return message
 
-    def __init__(self, fields: List[str]):
-        super(RedactingFormatter, self).__init__(self.FORMAT)
-        self.fields = fields
+# class RedactingFormatter(logging.Formatter):
+#     """ Redacting Formatter class
+#         """
+#     REDACTION = "***"
+#     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+#     SEPARATOR = ";"
 
-    def format(self, record: logging.LogRecord) -> str:
-        """Returns the log message obfuscated"""
-        return self.filter_datum(self.REDACTION,
-                                 super().format(record), self.SEPARATOR)
+#     def __init__(self, fields: List[str]):
+#         super(RedactingFormatter, self).__init__(self.FORMAT)
+#         self.fields = fields
 
-    def filter_datum(self, redaction: str,
-                     message: str, separator: str) -> str:
-        """Returns the log message obfuscated"""
-        for field in self.fields:
-            message = re.sub(fr'{field}=.*?{separator}',
-                             f'{field}={redaction}{separator}', message)
-        return message
+#     def format(self, record: logging.LogRecord) -> str:
+#         """Returns the log message obfuscated"""
+#         return self.filter_datum(self.REDACTION,
+#                                  super().format(record), self.SEPARATOR)
 
-
-def get_logger() -> logging.Logger:
-    """Returns a logger object"""
-    logger = logging.getLogger("user_data")
-    logger.setLevel(logging.INFO)
-    logger.propagate = False
-    stream_handler = logging.StreamHandler()
-    stream_handler.setFormatter(RedactingFormatter(PII_FIELDS))
-    logger.addHandler(stream_handler)
-    return logger
+#     def filter_datum(self, redaction: str,
+#                      message: str, separator: str) -> str:
+#         """Returns the log message obfuscated"""
+#         for field in self.fields:
+#             message = re.sub(fr'{field}=.*?{separator}',
+#                              f'{field}={redaction}{separator}', message)
+#         return message
 
 
-def get_db() -> mysql.connector.connection.MySQLConnection:
-    """
-    Returns a connector to the database
-    """
-    dbUser = os.getenv('PERSONAL_DATA_DB_USERNAME', 'root')
-    dbPassword = os.getenv('PERSONAL_DATA_DB_PASSWORD', '')
-    dbHost = os.getenv('PERSONAL_DATA_DB_HOST', 'localhost')
-    dbName = os.getenv('PERSONAL_DATA_DB_NAME')
-    return mysql.connector.connection.MySQLConnection(user=dbUser,
-                                                      password=dbPassword,
-                                                      host=dbHost,
-                                                      database=dbName)
+# def get_logger() -> logging.Logger:
+#     """Returns a logger object"""
+#     logger = logging.getLogger("user_data")
+#     logger.setLevel(logging.INFO)
+#     logger.propagate = False
+#     stream_handler = logging.StreamHandler()
+#     stream_handler.setFormatter(RedactingFormatter(PII_FIELDS))
+#     logger.addHandler(stream_handler)
+#     return logger
+
+
+# def get_db() -> mysql.connector.connection.MySQLConnection:
+#     """
+#     Returns a connector to the database
+#     """
+#     dbUser = os.getenv('PERSONAL_DATA_DB_USERNAME', 'root')
+#     dbPassword = os.getenv('PERSONAL_DATA_DB_PASSWORD', '')
+#     dbHost = os.getenv('PERSONAL_DATA_DB_HOST', 'localhost')
+#     dbName = os.getenv('PERSONAL_DATA_DB_NAME')
+#     return mysql.connector.connection.MySQLConnection(user=dbUser,
+#                                                       password=dbPassword,
+#                                                       host=dbHost,
+#                                                       database=dbName)
+
+
+# if __name__ == "__main__":
+#     db = get_db()
+#     cursor = db.cursor()
+#     cursor.execute("SELECT * FROM users;")
+#     formatter = RedactingFormatter(PII_FIELDS)
+
+#     for row in cursor:
+#         message = ""
+#         for i in range(len(row)):
+#             message += f"{cursor.column_names[i]}={row[i]};"
+#         print(formatter.)
+#     cursor.close()
+#     db.close()
